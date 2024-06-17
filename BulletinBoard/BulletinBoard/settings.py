d8647278477da31a8cb1e35f129910b58037b3a1
Andrey-Shum <shum75_84@mail.ru>
2024-06-17 03:58:20 +0300

    Первоначальная настройка без миграций и приложений

---
+++ BulletinBoard/BulletinBoard/settings.py
@@ -0,0 +1,125 @@
+"""
+Настройки Django для проекта BulletinBoard.
+
+Создано «django-admin startproject» с использованием Django 5.0.6.
+
+Дополнительную информацию об этом файле см.
+https://docs.djangoproject.com/en/5.0/topics/settings/
+
+Полный список настроек и их значений см.
+https://docs.djangoproject.com/en/5.0/ref/settings/
+"""
+
+from pathlib import Path
+
+# Создайте пути внутри проекта следующим образом: BASE_DIR/'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Настройки быстрого запуска разработки - непригодны для производства
+# См. https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/.
+
+
+SECRET_KEY = 'django-insecure-mkg#tly^fzk!$#ci0^q9=n^)jgkhv3l&+qxa%i!@jv#(q#67d%' # noqa
+
+
+DEBUG = True
+ALLOWED_HOSTS = []
+# DEBUG = False
+# ALLOWED_HOSTS = ['127.0.0.1']
+
+# Определение приложения
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'django_extensions',
+
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'BulletinBoard.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'BulletinBoard.wsgi.application'
+
+
+# База данных
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Проверка пароля
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # noqa
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', # noqa
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', # noqa
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Интернационализация
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = 'ru'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Статические файлы (CSS, JavaScript, изображения)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Тип поля первичного ключа по умолчанию
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
